6a278667c39b895aa52015c26b6b6d1ece61e156
sophieklm <sklm@live.co.uk>
2017-03-29 09:08:52 +0000

    update session2 challenge 11

--- session2/challenge/11_classes.rb
+++ session2/challenge/11_classes.rb
@@ -44,22 +44,18 @@ class BeerSong
   end
   
   def words(n)
-    n = {0 => "zero",1 => "one",2 => "two",3 => "three", 4 => "four", 5 => "five", 6 => "six",
-              7 => "seven", 8 => "eight", 9 => "nine", 10 => "ten", 11 => "eleven", 12 => "twelve",
-              13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen",
-              18 => "eighteen", 19 => "nineteen", 20 => "twenty", 30 => "thirty", 40 => "fourty",	
-              50 => "fifty", 60 => "sixty", 70 => "seventy", 80 => "eighty", 90 => "ninety"}
-		if self <= 20
-			words[self].capitalize
-		else
-			"#{words[(self.to_s.slice(0,1).to_i*10)].capitalize}-#{words[(self.to_s.slice(1,2).to_i)]}"
-		end
+    if n < 20 
+      %w(zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen)[n]
+    elsif n % 10 == 0 
+      %w(zero ten twenty thirty forty fifty sixty seventy eighty ninety)[n/10]
+    else 
+      "#{words n/10*10} - #{words n%10}".downcase
+    end
+    words.capitalize
   end
   
   def bottles(n)
-    n == 1 ? "#{n.words} bottle" : "#{n.words} bottles"
+    n == 1 ? "#{words(n)} bottle" : "#{words(n)} bottles"
   end
-  
 
-  
 end
